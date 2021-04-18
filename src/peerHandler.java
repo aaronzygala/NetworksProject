44eee8865c611df723c64e967a230f815e0e8571
Fran Luka Antoljak <fantoljak@ufl.edu>
2021-04-18 22:25:06 +0200

    Added thread functionality

--- src/peerHandler.java
+++ src/peerHandler.java
@@ -3,6 +3,7 @@ import java.io.*;
 import java.nio.*;
 import java.nio.channels.*;
 import java.util.*;
+import Peer;
 
 public class peerHandler {
     private Map<Integer, Client> peerMap;
@@ -12,50 +13,72 @@ public class peerHandler {
     boolean hasFile;
     Random random = new Random();
 
-    public void selectPreferredNeighbors(int NumberOfPreferredNeighbors) {
+    public synchronized void selectPreferredNeighbors(int NumberOfPreferredNeighbors) {
         
-        preferredNeighbors.clear();
+        long timer = commonData[1] * 1000;
 
-        for(int i = 0; i < NumberOfPreferredNeighbors; i++) {
-            
-            if(!hasFile) {
-                Client fastestDLRate = peerMap.get(0);
+        new Thread() {
+            public void run() {
+                try {
+                    preferredNeighbors.clear();
 
-                for(Map.Entry<Integer, Client> peer : peerMap.entrySet()) {
-                    if(peer.getValue().downloadRate == fastestDLRate.downloadRate
-                    && !preferredNeighbors.contains(peer.getValue())
-                    && interestedNeighbors.contains(peer.getValue())) {
-                        if(random.nextInt(2) == 0) // 0 or 1
-                            fastestDLRate = peer.getValue();
+                    for(int i = 0; i < NumberOfPreferredNeighbors; i++) {
+                        
+                        if(!hasFile) {
+                            Client fastestDLRate = peerMap.get(0);
+
+                            for(Map.Entry<Integer, Client> peer : peerMap.entrySet()) {
+                                if(peer.getValue().downloadRate == fastestDLRate.downloadRate
+                                && !preferredNeighbors.contains(peer.getValue())
+                                && interestedNeighbors.contains(peer.getValue())) {
+                                    if(random.nextInt(2) == 0) // 0 or 1
+                                        fastestDLRate = peer.getValue();
+                                }
+                                else if(peer.getValue().downloadRate > fastestDLRate.downloadRate
+                                && !preferredNeighbors.contains(peer.getValue())
+                                && interestedNeighbors.contains(peer.getValue()))
+                                    fastestDLRate = peer.getValue();
+                            }
+                            preferredNeighbors.add(fastestDLRate);
+                        }
+                        else {  // If peer has file, choose randomly
+                            int index = random.nextInt(peerMap.size());
+                            while(preferredNeighbors.contains(peerMap.get(index)) || !interestedNeighbors.contains(peerMap.get(index)))
+                                index = random.nextInt(peerMap.size());
+                            preferredNeighbors.add(peerMap.get(index));
+                        }
                     }
-                    else if(peer.getValue().downloadRate > fastestDLRate.downloadRate
-                    && !preferredNeighbors.contains(peer.getValue())
-                    && interestedNeighbors.contains(peer.getValue()))
-                        fastestDLRate = peer.getValue();
-                }
-                preferredNeighbors.add(fastestDLRate);
-            }
-            else {  // If peer has file, choose randomly
-                int index = random.nextInt(peerMap.size());
-                while(preferredNeighbors.contains(peerMap.get(index)) || !interestedNeighbors.contains(peerMap.get(index)))
-                    index = random.nextInt(peerMap.size());
-                preferredNeighbors.add(peerMap.get(index));
-            }
-        }
 
-        for(Map.Entry<Integer, Client> peer : peerMap.entrySet()) {
-            if(preferredNeighbors.contains(peer)) // && peer.choked == true
-                continue; // unchoke(peer)
-            else if(!preferredNeighbors.contains(peer)) // && peer.choked == false
-                continue; // choke(peer)
-        }
+                    for(Map.Entry<Integer, Client> peer : peerMap.entrySet()) {
+                        if(preferredNeighbors.contains(peer)) // && peer.choked == true
+                            continue; // unchoke(peer)
+                        else if(!preferredNeighbors.contains(peer)) // && peer.choked == false
+                            continue; // choke(peer)
+                    }
+                } catch(Exception e) {
+                    System.out.println(e);
+                }
+            }
+        };
+        Thread.sleep(timer);
     }
 
-    public void optimisticallySelectNeighbor() {
-        int index = random.nextInt(peerMap.size());
-        // while(peerMap.get(index).choked == false)
-            index = random.nextInt(peerMap.size());
-        // peerMap.get(index).unchoke();
-        optimisticallyUnchokedNeighbor = peerMap.get(index);        
+    public synchronized void optimisticallySelectNeighbor() {
+        long timer = commonData[2] * 1000;
+
+        new Thread() {
+            public void run() {
+                try {
+                    int index = random.nextInt(peerMap.size());
+                    // while(peerMap.get(index).choked == false)
+                        index = random.nextInt(peerMap.size());
+                    // peerMap.get(index).unchoke();
+                    optimisticallyUnchokedNeighbor = peerMap.get(index);  
+                } catch(Exception e) {
+                    System.out.println(e);
+                }
+            }
+        };
+        Thread.sleep(timer);      
     }
 }
