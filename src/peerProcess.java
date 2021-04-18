f79bc5d7b95588a8a2b2df9054842f325a1fe32f
Fran Luka Antoljak <fantoljak@ufl.edu>
2021-04-18 19:49:03 +0200

    Converted logic to byte arrays

--- src/peerProcess.java
+++ src/peerProcess.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.Dictionary;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -80,66 +81,56 @@ class Peer {
         }
     }
 
-
-    public int verifyHandshake(String handshake) {
-        String first18Bytes = handshake.substring(0, 18);
+    public int verifyHandshake(byte[] handshake) {
+        String first18Bytes = Arrays.toString(handshake).substring(0, 18);
         if(!first18Bytes.equals("P2PFILESHARINGPROJ"))
             return -1;
 
         for(int i = 18; i < 28; i++) {
-            if((int)handshake.charAt(i) != 0)
+            if(handshake[i] != 0)
                 return -1;
         }
 
-        int peerID = 0;
-        for(int i = 31; i > 27; i--) {
-            int temp = Character.getNumericValue(handshake.charAt(i));
-            peerID += temp * Math.pow(10,(31-i));
-        }
-
+        int peerID = Integer.parseInt(Arrays.toString(handshake).substring(28,31));
         return peerID;
     }
 
-    public void decodeMessage(int message) {
-        int length = message & 0xFFFFFFFF;
-        int type = message & (0xFF << 4*8);
+    public void decodeMessage(byte[] message) {
+        int length = Integer.parseInt(Arrays.toString(message).substring(0,4));
+        int type = message[4];
 
         switch(type) {
             case 0 : // choke
                 //choke(peerID);
+                //logger.ChokedNeighbor(peerID1, peerID2);
                 break;
             case 1 : // unchoke
                 //unchoke(peerID);
+                //logger.UnchokedNeighbor(peerID1, peerID2);
                 break;
             case 2 : // interested
                 interested(peerID);
                 break;
             case 3 : // not interested
                 notInterested(peerID);
+                //logger.NotInterestedMessage(peerID1, peerID2);
                 break;
             case 4 : // have
-                int have_pieceIndex = message & (0xFFFFFFFF << 5*8);
+                int indexField = Integer.parseInt(Arrays.toString(message).substring(5,9));
                 //have(peerID, have_pieceIndex);
+                //logger.HaveMessage(peerID1, peerID2, index);
                 break;
             case 5 : // bitfield
-                int payloadLength = length - 1; // 1 Byte for message type
-                int bitfieldMask = 0;
-                for(int i = 0; i < payloadLength * 8; i++)
-                    bitfieldMask |= (0x1 << i);
-                int serverBitfield = message & (bitfieldMask << 5*8);
+                int serverBitfield = Integer.parseInt(Arrays.toString(message).substring(5,message.length));
                 verifyBitfield(peerID, serverBitfield);
                 break;
             case 6 : // request
-                int req_pieceIndex = message & (0xFFFFFFFF << 5*8);;
+                int req_pieceIndex = Integer.parseInt(Arrays.toString(message).substring(5,9));
                 //request(peerID, req_pieceIndex);
                 break;
             case 7 : // piece
-                int pieceIndex = message & (0xFFFFFFFF << 5*8);;
-                int pieceLength = length - 2; // 2 Bytes for message type and piece index
-                int pieceMask = 0;
-                for(int i = 0; i < pieceLength; i++)
-                    pieceMask |= (0x1 << i);
-                int pieceData = message & (pieceMask << 6*8);
+                int pieceIndex = Integer.parseInt(Arrays.toString(message).substring(5,9));
+                int pieceData = Integer.parseInt(Arrays.toString(message).substring(9,message.length));
                 //piece(peerID, pieceIndex, pieceData);
                 break;
             default :
